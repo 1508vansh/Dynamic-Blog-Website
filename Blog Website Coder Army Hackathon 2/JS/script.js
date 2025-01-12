867e58ee58dc3b56466c1b7d8a9862d96c3df41c
1508vansh <vanshsoni13062005@gmail.com>
2025-01-13 02:13:18 +0530

    Structured all files and folders

--- Blog Website Coder Army Hackathon 2/JS/script.js
+++ Blog Website Coder Army Hackathon 2/JS/script.js
@@ -1,16 +1,16 @@
-//listener to locate to register page............
+// Listener to locate to register page
 let registerButton = document.querySelector('#Register');
-registerButton.addEventListener('click',()=>{
-   //To locate to user register page.......
-   window.location.href = 'register.html';
-}) 
+registerButton.addEventListener('click', () => {
+    // To locate to user register page
+    window.location.href = '../../register.html';
+});
 
-//listener to locate to createBlog page through dropdown 'create blog button'..........
+// Listener to locate to createBlog page through dropdown 'create blog button'
 let dropdownCreateBlogButton = document.getElementById('dropdown-createBlog-button');
-dropdownCreateBlogButton.addEventListener('click',()=>{
-    window.location.href = 'createBlog.html';
-})
+dropdownCreateBlogButton.addEventListener('click', () => {
+    window.location.href = '../../createBlog.html';
+});
 
-document.querySelector('#getstarted').addEventListener('click',()=>{
-    window.location.href = 'NotePage.html';
-})
+document.querySelector('#getstarted').addEventListener('click', () => {
+    window.location.href = '../../NotePage.html';
+});
